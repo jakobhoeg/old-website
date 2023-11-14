d8cd00b17b8242bb61965e4fd8878ec18214f871
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-11-14 08:48:07 +0100

    Bouncing arrow down

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { ArrowDownIcon } from "@radix-ui/react-icons";
 
 export default function Hero() {
   return (
-    <section className="w-full">
+    <section className="w-full ">
       <div className="flex flex-col items-center justify-center w-full h-full gap-6">
         <h1 className="text-center text-7xl max-w-5xl font-bold">
           Software developer with a passion for design
@@ -12,10 +13,11 @@ export default function Hero() {
           Trying to create cool stuff <br />
           since 1995.
         </h2>
-        <Button
-        className="rounded-full"
-        >
-            Get in touch
+        <Button className="rounded-full">Get in touch</Button>
+      </div>
+      <div className=" flex w-full items-center justify-center absolute bottom-10">
+        <Button size="icon" variant="ghost" className=" rounded-full animate-bounce">
+          <ArrowDownIcon className=" w-5 h-5 mx-auto text-muted-foreground" />
         </Button>
       </div>
     </section>
