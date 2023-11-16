2809f61199211dda55077b57af9baf44341870d4
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-11-16 17:56:28 +0100

    FIX

--- src/components/my-projects.tsx
+++ src/components/my-projects.tsx
@@ -5,7 +5,7 @@ export default function MyProjects() {
     <section className="w-full h-full flex justify-center">
       <div className="flex flex-col items-center px-5">
         <div className="text-center  space-y-1 md:space-y-4">
-          <p className="text-4xl md:text-6xl font-bold max-w-prose">Projects I've worked on</p>
+          <p className="text-4xl md:text-6xl font-bold max-w-prose">Projects I&apos;ve worked on</p>
           <p className="md:text-2xl prose text-zinc-400">
             Some of the projects I&apos;ve worked on, I am more proud of than others. <br />
             These will be showcased here.
