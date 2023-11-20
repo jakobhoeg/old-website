ed35689c2d9ddb5b561a409ae148e37855b0bb2b
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2023-11-20 14:41:39 +0100

    Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
   return (
     <main className="relative flex flex-col w-full min-h-screen">
       <Cursor />
-      <div className="scrollable-content -mt-10 md:mt-0 flex flex-col justify-center items-center h-screen">
+      <div className=" -mt-10 md:mt-0 flex flex-col justify-center items-center h-screen">
       <Navbar />
         <Hero />
       </div>
       <div className="h-[50vh] flex flex-col items-center ">
         <Skills />
       </div>
-      <div className="scrollable-content h-screen ">
+      <div className=" h-screen ">
         <MyProjects />
       </div>
     </main>
