9941cf9a105e4640949435d22fd77a4c8a76ffb8
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-01-30 15:05:18 +0100

    Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { HoverProvider } from "@/lib/hover-context";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
        </HoverProvider>
+          <Analytics />
       </body>
     </html>
   );
